202165b7093c531a9a483ffb48009f35e5eff288
ElJiva <angeljiva48@outlook.com>
2025-09-27 21:24:58 -0600

    Second Commit of Login Page

--- app/src/main/java/com/ivan/loginapp/screens/LoginScreen.kt
+++ app/src/main/java/com/ivan/loginapp/screens/LoginScreen.kt
@@ -4,12 +4,15 @@ package com.ivan.loginapp.screens
 
 import android.R.attr.text
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,13 +20,22 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,10 +46,14 @@ import com.ivan.loginapp.ui.theme.BlackBackground
 import com.ivan.loginapp.ui.theme.DarkWhiteBackground
 import com.ivan.loginapp.ui.theme.LoginAppTheme
 import com.ivan.loginapp.ui.theme.RegisterScreenRoute
+import androidx.compose.runtime.*
+
 
 
 @Composable
 fun LoginScreen(navController: NavController){
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
     Box  (
         modifier = Modifier
             .fillMaxSize()
@@ -91,23 +107,64 @@ fun LoginScreen(navController: NavController){
                 )
 
                 //Column  for Email and Password
-                Column (modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(2f)
-                    .padding(top = 70.dp)
-                    .background(Color.White)
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(2f)
+                        .padding(top = 70.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    TextField(
-                        value = text,
-                        onValueChange = {newText ->
-                            text = newText
-                        },
-                        label = {Text("Email")},
-                        placeholder = {Text("Enter your Email")}
-
-
+                    OutlinedTextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text("Email") },
+                        placeholder = { Text("Enter your Email") },
+                        shape = RoundedCornerShape(10.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.Transparent,
+                            unfocusedBorderColor = Color.Transparent
+                        ),
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(vertical = 8.dp)
+                            .fillMaxWidth(0.85f)
+                            .clip(RoundedCornerShape(15.dp))
+                            .background(Color.White)
                     )
 
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Password") },
+                        placeholder = { Text("Enter your Password") },
+                        visualTransformation = PasswordVisualTransformation(),
+                        shape = RoundedCornerShape(8.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.Transparent,
+                            unfocusedBorderColor = Color.Transparent
+                        ),
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(vertical = 8.dp)
+                            .fillMaxWidth(0.85f)
+                            .clip(RoundedCornerShape(15.dp))
+                            .background(Color.White)
+                    )
+
+                    Box(
+                        modifier = Modifier
+                            .padding(top = 24.dp)
+                            .fillMaxWidth(0.85f)
+                            .height(50.dp)
+                            .clip(RoundedCornerShape(topStart = 10.dp, bottomStart = 10.dp, bottomEnd = 10.dp))
+                            .background(Color.Black),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "Login",
+                            color = Color.White
+                        )
+                    }
                 }
 
                 //Nav for Resgiter Screen
